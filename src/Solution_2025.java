54ee9b1f4e515c147aa793d0ed50a16c5133560f
Sergey Pelemov <servp2008@gmaqil.com>
2020-06-10 16:31:29 +0300

    новый git sol2025

--- src/Solution_2025.java
+++ src/Solution_2025.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 public class Solution_2025 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        List<Integer> min = new ArrayList<>();
         int length = Integer.parseInt(br.readLine());
         if (length >= 1 && length <= 100) {
             StringTokenizer token = new StringTokenizer(br.readLine());
@@ -18,17 +18,26 @@ public class Solution_2025 {
                 mass.add(Integer.parseInt(token.nextToken()));
             }
             int k = Integer.parseInt(br.readLine());
+            if(k>=1 && k<=100) {
+                  for (int i = 0; i < k; i++) {
+                    token = new StringTokenizer(br.readLine());
+                    int[] index = new int[2];
+                    for (int j = 0; j < 2; j++) {
+                        index[j] = (Integer.parseInt(token.nextToken()));
+                    }
+                    if (index[0] >= 1 && index[0] <= index[1] && index[1] <= length) {
+                        if (index[0] == index[1]) {
+                            min.add(mass.get(index[0] - 1));
+                        } else {
+                            List<Integer> subList = mass.subList(index[0]-1, index[1]);
+                         //   subList.add(mass.get(index[1]));
+                            min.add(minInt(subList));
+                        }
+                    }
 
-            List<Integer> min = new ArrayList<>();
-            for (int i = 0; i < k; i++) {
-                token = new StringTokenizer(br.readLine());
-                int[] index = new int[2];
-                for (int j = 0; j < 2; j++) {
-                    index[j] = (Integer.parseInt(token.nextToken()));
                 }
-                List<Integer> subList = mass.subList(index[0], index[1]);
-                min.add(minInt(subList));
             }
+            for (Integer m : min) System.out.println(m);
         }
     }
 
