449286d12f482bcdeb266694b19e87653c3b5a2e
Sergey Pelemov <servp2008@gmaqil.com>
2020-06-11 10:35:49 +0300

    новый git sol2025

--- src/Person.java
+++ src/Person.java
@@ -8,6 +8,14 @@ public class Person {
         this.name = name;
     }
 
+    /**
+     *      * This method checks gender of persons. If genders are not equal - tries to marry.
+     *      * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     *      * @param person - new husband/wife for this person.
+     *      * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
+     *      
+     */
+
     public boolean marry(Person person) {
         if (person.man != this.man) {
             if (this.spouse != null || person.spouse != null) {
